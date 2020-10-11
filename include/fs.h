a978b554f3c74bc080135bd5fe47ef6c0a44ab6a
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-10-11 17:19:50 +0300

    Update fs.h

--- include/fs.h
+++ include/fs.h
@@ -1,1 +1,18 @@
+#ifndef _FS_H_
+#define _FS_H_ 1
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int mkdir(char *name);
+int mkfile(char *name);
+
+int rmdir(char *name);
+int rmfile(char *name);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
